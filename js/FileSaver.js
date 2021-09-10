6908c8b2794c3600a65a5f6d7554a3b869e99445
aygqq <aygqq@ya.ru>
2021-09-10 17:30:21 +0300

    delete some functions

--- js/FileSaver.js
+++ js/FileSaver.js
@@ -1,30 +1,30 @@
 /*! @source http://purl.eligrey.com/github/FileSaver.js/blob/master/FileSaver.js */
-var saveAs = saveAs || function(e) {
+var saveAs = saveAs || function (e) {
     "use strict";
     if (typeof e === "undefined" || typeof navigator !== "undefined" && /MSIE [1-9]\./.test(navigator.userAgent)) {
         return
     }
     var t = e.document,
-        n = function() {
+        n = function () {
             return e.URL || e.webkitURL || e
         },
         r = t.createElementNS("http://www.w3.org/1999/xhtml", "a"),
         o = "download" in r,
-        a = function(e) {
+        a = function (e) {
             var t = new MouseEvent("click");
             e.dispatchEvent(t)
         },
         i = /constructor/i.test(e.HTMLElement) || e.safari,
         f = /CriOS\/[\d]+/.test(navigator.userAgent),
-        u = function(t) {
-            (e.setImmediate || e.setTimeout)(function() {
+        u = function (t) {
+            (e.setImmediate || e.setTimeout)(function () {
                 throw t
             }, 0)
         },
         s = "application/octet-stream",
         d = 1e3 * 40,
-        c = function(e) {
-            var t = function() {
+        c = function (e) {
+            var t = function () {
                 if (typeof e === "string") {
                     n().revokeObjectURL(e)
                 } else {
@@ -33,7 +33,7 @@ var saveAs = saveAs || function(e) {
             };
             setTimeout(t, d)
         },
-        l = function(e, t, n) {
+        l = function (e, t, n) {
             t = [].concat(t);
             var r = t.length;
             while (r--) {
@@ -47,7 +47,7 @@ var saveAs = saveAs || function(e) {
                 }
             }
         },
-        p = function(e) {
+        p = function (e) {
             if (/^\s*(?:text\/\S*|application\/xml|\S*\/\S*\+xml)\s*;.*charset\s*=\s*utf-8/i.test(e.type)) {
                 return new Blob([String.fromCharCode(65279), e], {
                     type: e.type
@@ -55,20 +55,20 @@ var saveAs = saveAs || function(e) {
             }
             return e
         },
-        v = function(t, u, d) {
+        v = function (t, u, d) {
             if (!d) {
                 t = p(t)
             }
             var v = this,
                 w = t.type,
                 m = w === s,
-                y, h = function() {
+                y, h = function () {
                     l(v, "writestart progress write writeend".split(" "))
                 },
-                S = function() {
+                S = function () {
                     if ((f || m && i) && e.FileReader) {
                         var r = new FileReader;
-                        r.onloadend = function() {
+                        r.onloadend = function () {
                             var t = f ? r.result : r.result.replace(/^data:[^;]*;/, "data:attachment/file;");
                             var n = e.open(t, "_blank");
                             if (!n) e.location.href = t;
@@ -98,7 +98,7 @@ var saveAs = saveAs || function(e) {
             v.readyState = v.INIT;
             if (o) {
                 y = n().createObjectURL(t);
-                setTimeout(function() {
+                setTimeout(function () {
                     r.href = y;
                     r.download = u;
                     a(r);
@@ -111,11 +111,11 @@ var saveAs = saveAs || function(e) {
             S()
         },
         w = v.prototype,
-        m = function(e, t, n) {
+        m = function (e, t, n) {
             return new v(e, t || e.name || "download", n)
         };
     if (typeof navigator !== "undefined" && navigator.msSaveOrOpenBlob) {
-        return function(e, t, n) {
+        return function (e, t, n) {
             t = t || e.name || "download";
             if (!n) {
                 e = p(e)
@@ -123,7 +123,7 @@ var saveAs = saveAs || function(e) {
             return navigator.msSaveOrOpenBlob(e, t)
         }
     }
-    w.abort = function() {};
+    w.abort = function () { };
     w.readyState = w.INIT = 0;
     w.WRITING = 1;
     w.DONE = 2;
@@ -133,16 +133,56 @@ var saveAs = saveAs || function(e) {
 if (typeof module !== "undefined" && module.exports) {
     module.exports.saveAs = saveAs
 } else if (typeof define !== "undefined" && define !== null && define.amd !== null) {
-    define("FileSaver.js", function() {
+    define("FileSaver.js", function () {
         return saveAs
     })
 }
 
-function SaveAsFile(t,f,m) {
-  try {
-      var b = new Blob([t],{type:m});
-      saveAs(b, f);
-  } catch (e) {
-      window.open("data:"+m+"," + encodeURIComponent(t), '_blank','');
-  }
+function SaveAsFile(t, f, m) {
+    try {
+        var b = new Blob([t], { type: m });
+        saveAs(b, f);
+    } catch (e) {
+        window.open("data:" + m + "," + encodeURIComponent(t), '_blank', '');
+    }
+}
+
+// Function to download data to a file
+function SaveFile(data, filename, type) {
+    var file = new Blob([data], { type: type });
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+            url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function () {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+        }, 0);
+    }
+}
+
+function download(text, name, type) {
+    var a = document.getElementById("a");
+    a.style.display = "block";
+    var file = new Blob([text], { type: type });
+    a.href = URL.createObjectURL(file);
+    a.download = name;
+}
+
+function FileSaveJS(sourceText, fileIdentity) {
+    var workElement = document.createElement("a");
+    if ('download' in workElement) {
+        workElement.href = "data:" + 'text/plain' + "charset=utf-8," + escape(sourceText);
+        workElement.setAttribute("download", fileIdentity);
+        document.body.appendChild(workElement);
+        var eventMouse = document.createEvent("MouseEvents");
+        eventMouse.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        workElement.dispatchEvent(eventMouse);
+        document.body.removeChild(workElement);
+    } else throw 'File saving not supported for this browser';
 }
